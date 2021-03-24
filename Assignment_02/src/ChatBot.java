699d8c5cc4d76857a91fcce8ac7204138a62a80d
sara <sara@Tree_T-Rex.localdomain>
2021-03-23 18:18:15 -0700

    Implements the POS tagging toolkit.

--- Assignment_02/src/ChatBot.java
+++ Assignment_02/src/ChatBot.java
@@ -5,6 +5,12 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Properties;
+
+import edu.stanford.nlp.ling.*;
+import edu.stanford.nlp.pipeline.*;
+
+
 
 public class ChatBot {
     String name;
@@ -14,54 +20,59 @@ public class ChatBot {
     };
     public String sendPhrase(String phrase){
         phrase=dataClean(phrase);
+
         String ans = "";
         
         String[] stringArray = phrase.split(" ");
-        
+
+        // Send phrase to the POS tagger; Returns an ArrayList of possible keywords
+        ArrayList<String> list = pos(phrase);
+        String[] taggedData = new String[list.size()];
+        taggedData = list.toArray(taggedData);
+
         // Loop to find first keyword
-        for(int i = 0; i< stringArray.length; i++){
+        for(int i = 0; i< taggedData.length; i++){
             // If the first keyword is found call search() to find second keyword
-            if(stringArray[i].equals("experience")){
-                ans=search("experience", stringArray);
+            if(taggedData[i].equals("experience")){
+                ans=search("experience", taggedData);
                 break;
             };
-            if(stringArray[i].equals("travel")){
-                ans=search("travel", stringArray);
+            if(taggedData[i].equals("travel")){
+                ans=search("travel", taggedData);
                 break;
             };
-            if(stringArray[i].equals("goal")){
-                ans=search("goal", stringArray);
+            if(taggedData[i].equals("goal")){
+                ans=search("goal", taggedData);
                 break;
             };
-            if(stringArray[i].equals("hobby")){
-                ans=search("hobby", stringArray);
+            if(taggedData[i].equals("hobby")){
+                ans=search("hobby", taggedData);
                 break;
             };
-            if(stringArray[i].equals("school")){
-                ans=search("school", stringArray);
+            if(taggedData[i].equals("school")){
+                ans=search("school", taggedData);
                 break;
             };
-            if(stringArray[i].equals("volunteer")){
-                ans=search("volunteer", stringArray);
+            if(taggedData[i].equals("volunteer")){
+                ans=search("volunteer", taggedData);
                 break;
             };
-            if(stringArray[i].equals("salary")){
-                ans=search("salary", stringArray);
+            if(taggedData[i].equals("salary")){
+                ans=search("salary", taggedData);
                 break;
             };
-            if(stringArray[i].equals("skills")){
-                ans=search("skills", stringArray);
+            if(taggedData[i].equals("skills")){
+                ans=search("skills", taggedData);
                 break;
             };
-            if(stringArray[i].equals("training")){
-                ans=search("training", stringArray);
+            if(taggedData[i].equals("training")){
+                ans=search("training", taggedData);
                 break;
             };
-            if(stringArray[i].equals("certifications")){
-                ans=search("certifications", stringArray);
+            if(taggedData[i].equals("certifications")){
+                ans=search("certifications", taggedData);
                 break;
             };
- 
         };
 
         //If none of the keywords were found look in the miscellaneous csv for generic questions
@@ -111,4 +122,20 @@ public class ChatBot {
         cleanedPhrase=cleanedPhrase.replace("?","").replace(".","").replace(",","").replace("!","");
         return cleanedPhrase;
     };
+
+    public ArrayList<String> pos(String text){
+
+        ArrayList<String> possibleKeywords = new ArrayList<>();
+        Properties property = new Properties();
+        property.setProperty("annotators", "tokenize,ssplit,pos");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(property);
+        CoreDocument document = pipeline.processToCoreDocument(text);
+        for (CoreLabel token : document.tokens()) {
+            //System.out.println(token.word() + "   " + token.tag());
+            if (token.tag().equals("NN") || token.tag().equals("NNS") || token.tag().equals("JJ") || token.tag().equals("JJS") || token.tag().equals("CD") || token.tag().equals("VB") || token.tag().equals("UH") || token.tag().equals("WRB") || token.tag().equals("RB") || token.tag().equals("DT")){
+                possibleKeywords.add(token.word());
+            }
+        }
+        return possibleKeywords;
+    }
 }
